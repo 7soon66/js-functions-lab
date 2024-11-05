10e48d7e2296e9fbae1fd7b2fca8782d4d1b857b
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-11-05 15:26:08 +0300

    excersise nine is done

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 /*
-Exercise 8: convertTemperature()
+Exercise 9: basicCalculator()
 
-Write a function named convertTemperature. 
-It takes two arguments: a temperature and a string representing the 
-scale ('C' for Celsius, 'F' for Fahrenheit). 
-Convert the temperature to the other scale.
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
 
-Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
-Example: convertTemperature(32, 'F') should return 0 (Celsius).
+Example: basicCalculator(10, 5, 'subtract') should return 5.
 
 Complete the exercise in the space below:
 */
 
-const convertTemperature = (temperature, scaleOfTempreture) => {
-  if (scaleOfTempreture == 'c') {
-    return temperature * 2.8
-  } else if (scaleOfTempreture == 'f') {
-    return temperature * 1
+const basicCalculator = (num1, num2, operation) => {
+  if (operation == 'add') {
+    return num1 + num2
+  } else if (operation == 'substract') {
+    return num1 - num2
+  } else if (operation == 'multiply') {
+    return num1 * num2
+  } else if (operation == 'divide') {
+    return num1 / num2
   } else {
-    return 'please enter a c or f'
+    console.log('sorry you enter a wrong operation change it please!!')
   }
 }
-console.log('Exercise 8 Result:', convertTemperature(32, 'f'))
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, 'multiply'))
