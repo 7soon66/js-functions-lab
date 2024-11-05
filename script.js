eb316a4605c5e29a219a1697ace1a01906ade4a2
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-11-05 15:15:36 +0300

    excersise eight is done

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 /*
-Exercise 7: calculateTip()
+Exercise 8: convertTemperature()
 
-Create a function called calculateTip. It should take two arguments: 
-the bill amount and the tip percentage (as a whole number). 
-The function should return the amount of the tip.
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
 
-Example: calculateTip(50, 20) should return 10.
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
 
 Complete the exercise in the space below:
 */
 
-const calculateTip = (billAmount, tepPercantige) => {
-  return billAmount % tepPercantige
+const convertTemperature = (temperature, scaleOfTempreture) => {
+  if (scaleOfTempreture == 'c') {
+    return temperature * 2.8
+  } else if (scaleOfTempreture == 'f') {
+    return temperature * 1
+  } else {
+    return 'please enter a c or f'
+  }
 }
-
-console.log('Exercise 7 Result:', calculateTip(50, 20))
+console.log('Exercise 8 Result:', convertTemperature(32, 'f'))
