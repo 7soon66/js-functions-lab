1d0951a3e8c797a4e25d5f762dff5b3ef7e02d52
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-11-05 12:48:29 +0300

    excersise four is done

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const isCharAVowl = (vowl) => {
-  if (
-    vowl === 'a' ||
-    vowl === 'e' ||
-    vowl === 'i' ||
-    vowl === 'o' ||
-    vowl === 'u'
-  ) {
-    return 'true'
-  } else {
-    return 'false'
-  }
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (name, domain) => {
+  return name + domain
 }
 
-console.log('Exercise 3 Result:', isCharAVowl('b'))
+console.log('Exercise 4 Result:', generateEmail('johnsmith', 'example.com'))
